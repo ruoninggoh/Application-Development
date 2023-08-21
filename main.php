3db0722f04dace46a37689970e20516d54e3401f
jiauting <jiautingt@gmail.com>
2023-08-21 22:22:01 +0800

    initial

---
+++ main.php
@@ -0,0 +1,27 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>UniHealth</title>
+    <link rel="stylesheet" href="css/landing.css">
+    
+</head>
+
+<body>
+<?php include("css/landingHeader.html"); ?>
+        <div class="mainIntro">
+            <h1 class="">HEALTH IS THE MAIN THING <br>FOR US</h1> <br>
+            <h5><i>Your Health is our priority.<br>
+                    We also provide a best service, Make your appointment now.</i></h5>
+        </div>
+        <br>
+        <div class="center-container">
+            <form action="">
+                <button type="button" class="btn btn-black">Make Appointment</button>
+            </form>
+        </div>
+
+</body>
+
+</html>
