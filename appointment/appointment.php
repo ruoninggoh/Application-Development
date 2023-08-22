9b600f959ab2b54d0763f469191e2e312644acae
JasonTan02 <103154789+JasonTan02@users.noreply.github.com>
2023-08-22 17:41:28 +0800

    location updated

--- appointment/appointment.php
+++ appointment/appointment.php
@@ -2,7 +2,7 @@
     session_start();
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         if (!isset($_SESSION["userID"])){
-            header("Location:signin_form.php");
+            header("Location:userdashboard.php");
             exit();
         }
     }
