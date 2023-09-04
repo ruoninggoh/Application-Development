888349a295b2ee827a9c1fd73d86c8f08f22c660
jiauting <jiautingt@gmail.com>
2023-09-05 00:41:27 +0800

    manage user

--- admin/userManagement.php
+++ admin/userManagement.php
@@ -31,8 +31,8 @@ mysqli_close($con);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>User Management</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="manage_users.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="manageuser.css">
+    <?php include("./adminHeader.html");?>
 </head>
 
 <body>
