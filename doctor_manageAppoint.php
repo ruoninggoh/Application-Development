f1a640473b73fe6523d0c3fc74fd6545390f4530
JasonTan02 <103154789+JasonTan02@users.noreply.github.com>
2023-09-04 12:53:15 +0800

    table added

--- doctor_manageAppoint.php
+++ doctor_manageAppoint.php
@@ -5,6 +5,19 @@
         <link rel="stylesheet" type="text/css" href="manageAppoint.css">
     </head>
     <body>
+        <?php
+            $conn = mysqli_connect("localhost", "root", "", "unihealth");
+            if(!$conn){
+                die("Connection failed:" . mysqli_connect_errno());
+            }
+
+            $sql = "SELECT a.appointID, u.username, a._date, a._time, a.reason
+                    FROM appointment a
+                    INNER JOIN user u ON a.user_id = u.userID
+                    WHERE a.requestStatus = 'Pending'";
+
+            $result = mysqli_query($conn, $sql);
+        ?>
         <div class="menu-bar">
             <div class="bus-selector">
                 <div class="tabs">
@@ -12,9 +25,39 @@
                     <div class="tab" onclick="toggleTab('My')" data-tab="My">My</div>
                 </div>
                 <div class="tab-content active" data-tab="Pending">
-                    <p>
-                        Testing pending tab content display
-                    </p>
+                <table>
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th colspan="2">Name</th>
+                                <th>Date</th>
+                                <th>Time</th>
+                                <th>Details</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $index = 0;
+                                while ($row = mysqli_fetch_assoc($result)){
+                                    echo "<tr>";
+                                    echo "++$index";
+                                    echo "<td colspan='2'>" . $row['username'] . "</td>";
+                                    echo "<td>" . $row['_date'] . "</td>";
+                                    echo "<td>" . $row['_time'] . "</td>";
+                                    echo "<td><button class='details-button' data-reason='" . $row['reason'] . "'><img src='details.png' alt='Details'></button></td>";
+                                    echo "<td></td>";
+                                    echo "</tr>";
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                    <div class="modal" id="reasonModal">
+                        <div class="modal-content">
+                            <span id="closeModal" style="float: right; cursor: pointer;">&times;</span>
+                            <p id="reasonText"></p>
+                        </div>
+                    </div>
                 </div>
                 <div class="tab-content" data-tab="My">
                     <p>
@@ -50,6 +93,18 @@
                 var selectedTab = document.querySelector('.tab[data-tab="' + tabText + '"]');
                 selectedTab.classList.add('active');
             }
+
+            $(document).ready(function(){
+                $('.details-button').click(function(){
+                    var reason = $(this).data('reason');
+                    $('#reasonText').text(reason);
+                    $('#reasonModal').css('display', 'block');
+                });
+
+                $('#closeModal').click(function(){
+                    $('#reasonModal').css('display', 'none');
+                });
+            });
         </script>
     </body>
 </html>
