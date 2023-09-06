aa3134ba455b7627883514fd23df0bc5bbb1cb11
chloe <gohning@graduate.utm.my>
2023-09-06 10:57:22 +0800

    update dashboard table name

--- doctor/dashboard.php
+++ doctor/dashboard.php
@@ -79,7 +79,7 @@ session_start();
             $numUser=mysqli_num_rows($userrow);
 
 
-            $doctorrow=mysqli_query($con,"select * from doctor");
+            $doctorrow=mysqli_query($con,"select * from user WHERE role = 'Doctor'");
             $numDoc=mysqli_num_rows($doctorrow);
 
             $numbooking=0;
