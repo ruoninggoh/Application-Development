a2a1ffb4d4d3c790451165c40c98737b6b1b8362
chloe <gohning@graduate.utm.my>
2023-09-12 14:58:52 +0800

    desc

--- doctor/insertdiagnose.php
+++ doctor/insertdiagnose.php
@@ -134,7 +134,7 @@ if(isset($_GET['appointID'])){
 
         <div class="form second">
           <div class="details mc">
-            <span class="title"><b>Sijil Cuti Sakit Purpose</b> </span>
+            <span class="title"><b>Sijil Cuti Sakit Purpose</b> [If there is only 1day just insert the same date]</span>
 
             <div class="fields" style="margin:30px;">
 
