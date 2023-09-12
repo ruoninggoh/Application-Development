e06a642b7640ea18b599596a1867e3437ced50a1
chloe <gohning@graduate.utm.my>
2023-09-12 14:57:10 +0800

    .

--- doctor/insertdiagnose.php
+++ doctor/insertdiagnose.php
@@ -134,7 +134,7 @@ if(isset($_GET['appointID'])){
 
         <div class="form second">
           <div class="details mc">
-            <span class="title"><b>Sijil Cuti Sakit Purpose</b> [No need to fill in If there is not necessary to have it]</span>
+            <span class="title"><b>Sijil Cuti Sakit Purpose</b> </span>
 
             <div class="fields" style="margin:30px;">
 
